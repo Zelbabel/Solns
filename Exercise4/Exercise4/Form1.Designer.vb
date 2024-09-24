fdf4f83ed6b7b9958c051787a529633ce7dc9a79
Zeyewa Z Felix <zeyewafelix@gmail.com>
2024-09-24 16:41:19 +0300

    Add files via upload

---
+++ Exercise4/Exercise4/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnnested = New Button()
+        SuspendLayout()
+        ' 
+        ' btnnested
+        ' 
+        btnnested.Location = New Point(224, 137)
+        btnnested.Name = "btnnested"
+        btnnested.Size = New Size(301, 91)
+        btnnested.TabIndex = 0
+        btnnested.Text = "Count"
+        btnnested.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnnested)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnnested As Button
+
+End Class
---
+++ Exercise4/Exercise4/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnnested_Click(sender As Object, e As EventArgs) Handles btnnested.Click
+        For i As Integer = 1 To 10
+            For j As Integer = 1 To 10
+                MessageBox.Show(i & " * " & j)
+            Next
+        Next
+    End Sub
+End Class
